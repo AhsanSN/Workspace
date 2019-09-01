a0edc6b3f48594985a2765ebed0b4c49694ea016
Syed Ahsan Ahmed <snahmed1998@gmail.com>
2019-09-01 18:15:09 +0500

    int

---
+++ al-slider-2/al-slider/js/main.js
@@ -0,0 +1,8 @@
+// $('#carouselExampleIndicators').bind('mousewheel', function(e) {
+//     if (e.originalEvent.wheelDelta / 120 > 0) {
+//         $(this).carousel('next');
+//     } else {
+//         $(this).carousel('prev');
+//     }
+// });
+
